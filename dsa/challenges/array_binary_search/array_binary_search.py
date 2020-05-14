4459f28e11107fd3206758aa983499faa0af3bc3
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-05-13 22:03:11 -0700

    read me updated

--- dsa/challenges/array_binary_search/array_binary_search.py
+++ dsa/challenges/array_binary_search/array_binary_search.py
@@ -1,2 +1,5 @@
 def binary_search(arr ,key):
-  return arr[key]
+  if key < len(arr) and key >= 0:
+    return arr[key]
+  elif key >= len(arr) or key < 0:
+    return -1
--- tests/challenges/test_array_binary_search.py
+++ tests/challenges/test_array_binary_search.py
@@ -1,5 +1,10 @@
 from dsa.challenges.array_binary_search.array_binary_search import binary_search
 
+def test_binary_search_negative():
+  actual = binary_search([1, 2, 3, 4, 5], -2)
+  expected = -1
+  assert actual == expected
+
 def test_binary_search_zero():
   actual = binary_search([1, 2, 3, 4, 5], 0)
   expected = 1
@@ -10,7 +15,7 @@ def test_binary_search_two():
   expected = 3
   assert actual == expected
 
-def test_binary_search_four():
-  actual = binary_search([15, 16 , 17, 18, 19, 20], 4)
-  expected = 19
+def test_binary_search_seven():
+  actual = binary_search([15, 16 , 17, 18, 19, 20], 7)
+  expected = -1
   assert actual == expected
