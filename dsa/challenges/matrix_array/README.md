f2cdb444ba4bb5d7bd8fb1b2e317dbd5c611a2dd
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-05-14 21:41:14 -0700

    still doing stuff

---
+++ dsa/challenges/matrix_array/README.md
@@ -0,0 +1,16 @@
+# Challenge Summary
+matrix array : insert integer to middle of array
+
+## Challenge Description
+Ask the candidate to write a function to add up the sum of each row in a matrix of arbitrary size, and return an array with the appropriate values.
+Avoid utilizing any of the built-in methods available to your language.
+Don’t let the candidate get scared by the term “matrix”… It’s just an array of arrays.
+The matrix will always be full of integers.
+Negative values are possible.
+All nulls will be counted as zeros.
+
+## Approach & Efficiency
+
+
+## Solution
+[matrix array whiteboard image](/assets/matrix_array.png)
---
+++ dsa/challenges/matrix_array/matrix_array.py
@@ -0,0 +1,44 @@
+# def matrix_array(arr):
+  # print(arr)
+  # small_arr = []
+  # for i in range(len(arr)): # [] [] []
+    # small_arr.append[i]
+    # print(arr[i])
+
+    # for j in range(len(arr[i])): #small_arr: # [1,2,3]
+      # add_arr = []
+      # arr[i][0] = (arr[i][0] + arr[i][1])
+      # print(arr[i][0])
+      # print(arr[i][j])
+      # print(arr[i][j])
+      
+      # small_arr = []
+      # if j < in :
+        # add = arr[0]+ arr[1]
+        # small_arr.append(add)
+      #   print(arr)
+      # if j == len(arr)+1:
+      #   break
+matrix_array([ [1,2,3], [4,5,6] ]) # [ 6, 15 ]
+
+def matrix_array(arr):
+  output_arr = [] # [ rowVal, rowVal] 
+  for i in range(len(arr)):
+    print(arr[i])
+    for j in range(len(arr[i])):
+      print(arr[i][j])
+  return output_arr
+
+# def matrix_array(arr):
+  # for i in arr:
+  #   print(arr[i])
+
+# matrix_array( [ [1,2,3], [4,5,6] ] )
+
+
+
+# arr = [ [1,2,3], [4,5,6], [7,8,9] ]
+# print(arr[0])                   #[1,2,3]
+# print(arr[0][0])                #1
+# print(arr[0][0] + arr[0][1])    #1+2 = 3
+# print(arr[0][0] + arr[1][2])    #1+5 = 7
