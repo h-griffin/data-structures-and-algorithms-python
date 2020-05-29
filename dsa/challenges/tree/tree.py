4d6ca9996ad448cceaf440fa491f8fef65c6e006
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-05-29 15:12:25 -0700

    tree shells

---
+++ dsa/challenges/tree/tree.py
@@ -0,0 +1,32 @@
+class Node:
+    pass
+
+class BinaryTree:
+    def __init__(self):
+        pass
+
+    def pre_order(self):
+        """returns array of values ordered _"""
+        pass
+
+    def in_order(self):
+        """returns array of values ordered _"""
+        pass
+
+    def posr_order(self):
+        """returns array of values ordered _"""
+        pass
+
+class BinarySearchTree:
+    def __init__(self):
+        pass
+
+    def add(self, value):
+        """takes in a value, adds a new Node with that value to the correct location in binary search tree"""
+        pass
+
+    def contains(self, value):
+        """takes in a value, returns boolean if value is in tree"""
+        pass
+
+#last line
---
+++ tests/challenges/test_tree.py
@@ -0,0 +1,37 @@
+def test_empty_tree():
+    # actual =
+    # expected =
+    # assert actual == expected
+    pass
+
+def test_single_root():
+    # actual =
+    # expected =
+    # assert actual == expected
+    pass
+
+def test_left_right():
+    # actual =
+    # expected =
+    # assert actual == expected
+    pass
+
+def test_pre():
+    # actual =
+    # expected =
+    # assert actual == expected
+    pass
+
+def test_in():
+    # actual =
+    # expected =
+    # assert actual == expected
+    pass
+
+def test_post():
+    # actual =
+    # expected =
+    # assert actual == expected
+    pass
+
+#last line
