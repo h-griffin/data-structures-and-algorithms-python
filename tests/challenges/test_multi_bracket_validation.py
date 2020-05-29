7c4fec21c2765c2f4954e6cea96c27ed91b5457c
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-05-28 18:10:36 -0700

    shell for optional challenge

---
+++ tests/challenges/test_multi_bracket_validation.py
@@ -0,0 +1,8 @@
+
+from dsa.challenges.multi_bracket_validation.multi_bracket_validation import multi_bracket_validation
+
+def test_():
+    pass
+    # actual =
+    # expected =
+    # assert actual == expected
