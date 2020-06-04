ffd04628bc401cae6affae5817369fe8f34b83d6
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-06-03 19:02:34 -0700

    max val complete, no readme or whiteboard

---
+++ dsa/challenges/find_maximum_binary_tree/find_maximum_binary_tree.py
@@ -0,0 +1,16 @@
+from collections import deque
+from dsa.challenges.fizz_buzz_tree.fizz_buzz_tree import Node, BinarySearchTree, BinaryTree, Queue
+
+
+def find_maximum_value(tree):
+    """Takes in a tree as a single argument. returns greatest Node value in tree."""
+    collection = tree.breadth_first() #list
+    max_val = 0
+
+    for i in collection:
+        if i > max_val:
+            max_val = i
+        else:
+            print('doing nothing')
+
+    return max_val
---
+++ tests/challenges/test_find_maximum.py
@@ -0,0 +1,34 @@
+from dsa.challenges.find_maximum_binary_tree.find_maximum_binary_tree import find_maximum_value
+from collections import deque
+from dsa.challenges.fizz_buzz_tree.fizz_buzz_tree import Node, BinarySearchTree, BinaryTree, Queue
+import pytest
+
+def test_max_val():
+    tree = BinaryTree()
+    tree.add(1)
+    tree.add(5)
+    tree.add(4)
+    tree.add(7)
+    actual = find_maximum_value(tree)
+    expected = 7
+    assert actual == expected
+
+def test_max_val_zero():
+    tree = BinaryTree()
+    tree.add(-1)
+    tree.add(-5)
+    tree.add(0)
+    tree.add(-7)
+    actual = find_maximum_value(tree)
+    expected = 0
+    assert actual == expected
+
+# def test_max_val_negative():
+#     tree = BinaryTree()
+#     tree.add(-1)
+#     tree.add(-5)
+#     tree.add(-4)
+#     tree.add(-7)
+#     actual = find_maximum_value(tree)
+#     expected = -1
+#     assert actual == expected
