cac2f77e9517f039d6b33a0f3506bfa8af91d54a
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-06-05 19:48:45 -0700

    tree whitebpard and testing

--- dsa/challenges/tree/README.md
+++ dsa/challenges/tree/README.md
@@ -1,9 +1,32 @@
 # Challenge Summary
 
 ## Challenge Description
+Create binary search tree class that takes in a binary tree and traverses depth first
 
 ## Approach & Efficiency
-
+Create a binary tree class (this will be the super class of BST)
+Create a method for traversal
+	Create an empty variable for the output , a list
+	Create a method called walk that will traverse through the tree (pre order below)
+		Append the node value  (root that called walk)
+		Call the walk method again for the left value
+		Call the walk method again for the right value
+	Call the walk metod with the root
+Return the output
+Create a binary search tree class that inherits the binary tree clas
+Create a function to add values to the tree
+	Define a walk method that takes in a node to find and a new node to add
+		If the new node is greater than the root go left
+			If there is no left add to left
+			If there is a left, walk left and to the lefts left
+		Else go right
+			If there is no right add to right
+			If there is right, walk right adn add to rights right
+		If there is no root (empty tree)
+			Set new node to root
+Define a contains method that takes in a value and searches the tree
+	Run thte tree though a traversal method returning a list of values
+		Iterate through list and return true or false
 
 ## Solution
-![ whiteboard image](/assets/.png)
+![ whiteboard image](/assets/tree.png)
--- tests/challenges/test_tree.py
+++ tests/challenges/test_tree.py
@@ -1,51 +1,71 @@
 from dsa.challenges.tree.tree import BinaryTree, BinarySearchTree, Node
 
-def test_empty_tree():
-    # actual = BinarySearchTree()
-    # expected = BinarySearchTree.root=None
-    # assert actual == expected
-    pass
+def test_tree():
+    assert BinaryTree()
 
 def test_single_root():
-    # actual = BinarySearchTree.add(1)
-    # expected =
-    # assert actual == expected
-    pass
+    bst = BinarySearchTree()
+    bst.add(1)
+    actual = bst.root.value
+    expected = 1
+    assert actual == expected
 
 
 def test_left_right():
-    # actual =
-    # expected =
-    # assert actual == expected
-    pass
+    bst = BinarySearchTree()
+    bst.add(2)
+    bst.add(1)
+    bst.add(3)
+    right = bst.root.right.value
+    expected_right = 3
+    left = bst.root.left.value
+    expected_left = 1
+    assert right == expected_right
+    assert left == expected_left
 
 def test_pre():
-    # actual =
-    # expected =
-    # assert actual == expected
-    # bst = BinarySearchTree()
-    # bst.add(4)
-    # bst.add(7)
-    # bst.add(5)
-    # bst.add(9)
-    # bst.add(2)
-    # bst.add(30)
-    # bst.add(-1)
-    # bst.pre_order()
-    pass
+    bst = BinarySearchTree()
+    bst.add(1)
+    bst.add(2)
+    bst.add(3)
+    bst.add(4)
+    bst.add(5)
+    actual = bst.pre_order()
+    expected = [1, 2, 3, 4, 5]
+    assert actual == expected
 
 def test_in():
-    # actual =
-    # expected =
-    # assert actual == expected
-    pass
+    bst = BinarySearchTree()
+    bst.add(85)
+    bst.add(70)
+    bst.add(75)
+    bst.add(45)
+    bst.add(100)
+    bst.add(90)
+    bst.add(110)
+    actual = bst.in_order()
+    expected = [45, 70, 75, 85, 90, 100, 110]
+    assert actual == expected
 
 def test_post():
-    # actual =
-    # expected =
-    # assert actual == expected
-    pass
+    bst = BinarySearchTree()
+    bst.add(50)
+    bst.add(60)
+    bst.add(55)
+    bst.add(40)
+    bst.add(45)
+    actual = bst.post_order()
+    expected = [45, 40, 55, 60, 50]
+    assert actual == expected
+
+def test_contains():
+    bst = BinarySearchTree()
+    bst.add(1)
+    bst.add(2)
+    bst.add(3)
+    actual = bst.contains(2)
+    expected = True
+    assert actual == expected
 
 
-#last line
-#last line
+
