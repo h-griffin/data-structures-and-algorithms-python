b257b63d2b056f474be4b9cc1bbcdba27efd444d
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-06-17 20:42:06 -0700

    code and testing done

---
+++ dsa/challenges/blog_quick_sort/blog_quick_sort.py
@@ -0,0 +1,26 @@
+def quick_sort(alist, left, right):
+    """sorts array using partition and pivot value"""
+    if left < right:
+        position = partition(alist, left, right)
+        quick_sort(alist, left, position - 1)
+        quick_sort(alist, position + 1, right)
+
+def partition(alist, left, right):
+    #set pivot as least unsorted value
+    pivot = alist[right]
+
+    low = left - 1
+
+    for i in range(left, right):
+        if alist[i] <= pivot:
+            low += 1
+            swap(alist, i, low)
+
+    swap(alist, right, low + 1)
+    # return the pivot index point
+    return low + 1
+
+def swap(alist, i, low):
+    temp = alist[i]
+    alist[i] = alist[low]
+    alist[low] = temp
