8e0a2f57465d9db34cf5ec15b71ad03d97122e4b
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-07-12 01:22:02 -0700

    get edges graph testing and readme

---
+++ dsa/data_structures/graph/README.md
@@ -0,0 +1,13 @@
+# graph
+
+## get edge
+get edge returns a given vertexes edges/neighbors, and how much that connection is weighted.
+
+
+
+![get edges graph](assets/get_edges_graph.png)
+
+
+
+
+
--- dsa/data_structures/graph/graph.py
+++ dsa/data_structures/graph/graph.py
@@ -1,3 +1,15 @@
+class Vertex:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    def __str__(self):
+        return self.value
+
+
+class Edge:
+    def __init__(self, vertex, weight=1):
+        self.vertex = vertex
+        self.weight = weight
 
 
 
@@ -7,8 +19,8 @@ class Graph:
         self._adjacency_list = {}
 
     def __len__(self):
-    """returns length of key list, which gives number of keys"""
-    return len(self._adjacency_list)
+        """returns length of key list, which gives number of keys"""
+        return len(self._adjacency_list)
 
     # vertex = node
     def add_vertex(self, value):
@@ -17,7 +29,8 @@ class Graph:
         self._adjacency_list[v] = [] # list of neighbors
         return v
 
-    def add_edge(self, start_vertex, end_vertex, weight=0): # add neighbor
+    #   add neighbor
+    def add_edge(self, start_vertex, end_vertex, weight=0):
         """takes in two vertexes already existing in the graph, with an optional weight. returns nothing"""
         if start_vertex not in self._adjacency_list:
             raise KeyError('start vertex not in graph')
@@ -30,18 +43,33 @@ class Graph:
         adjacencies = self._adjacency_list[start_vertex] # list of neighbors
         adjacencies.append(edge)
 
-
-    def get_vertices(self):
+    #   get node
+    def get_vertex(self):
         """returns all keys in graph"""
         return self._adjacency_list.keys()
 
+    # def get_neighbors(self, vertex):
+    #     """returns all edges"""
+    #     return self._adjacency_list.get(vertex, [])       # <<<<<<
+
+    #   get edge   + weight
     def get_neighbors(self, vertex):
-        """returns all edges"""
-        return self._adjacency_list.get(vertex, [])
+        """ Takes in a vertex/node and returns a collection of edges connected to the given vertex/node as well as the weight of the connection.
+        """
+        collection = []
+        connections =  self._adjacency_list.get(vertex, [])         # <<<<<
+
+        for neighbor in connections:
+            holder = {}
+            holder[neighbor] = neighbor.weight
+            collection.append(holder)
+
+        return collection
 
     def size(self):
         """ Returns the total number of vertices/nodes in the graph"""
         return len(self.graph) if len(self.graph) > 0 else None
+
 
     def breadth_first(self, vertex):
         """ Takes in a node/vertex and performs a breadth first traversal of the graph. This will return a collection of the nodes/vertices within the graph from a breadth first traversal perspective of the given node/vertex.
@@ -62,18 +90,3 @@ class Graph:
                     breadth.enqueue(child.vertex)
 
         return nodes
-
-class Vertex:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-    def __str__(self):
-        return self.value
-
-
-class Edge:
-    def __init__(self, vertex, weight=1):
-        self.vertex = vertex
-        self.weight = weight
-
-
