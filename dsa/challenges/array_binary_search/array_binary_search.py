dce2ea43ed0efce9ae3786e718182a1e52298199
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-07-23 19:28:32 -0700

    srray comments

--- dsa/challenges/array_binary_search/array_binary_search.py
+++ dsa/challenges/array_binary_search/array_binary_search.py
@@ -1,28 +1,3 @@
-# def binary_search(arr, key):
-#     if key < len(arr) and key >= 0:
-#         return arr[key]
-#     elif key >= len(arr) or key < 0:
-#         return -1
-
-
-# def binary_search(arr, key):
-#     lower_bound = 0
-#     upper_bound = len(arr) - 1
-
-#     while lower_bound <= upper_bound:
-#         midpoint = (upper_bound + lower_bound) / 2
-#         key_at_midpoint = arr[midpoint]
-
-#         if key < key_at_midpoint:
-#             upper_bound = midpoint - 1
-#         elif key > key_at_midpoint:
-#             lower_bound = midpoint + 1
-#         elif key == key_at_midpoint:
-#             return midpoint
-#         break
-
-
-# print(binary_search([1, 2, 3, 4, 5], 2))
 
 def binary_search(arr,x):
     low = 0
@@ -31,13 +6,13 @@ def binary_search(arr,x):
 
     while low <= high:
         mid = (high + low) // 2
-        if arr[mid] < x: #is x at mid
+        if arr[mid] < x:    #is x at mid
             low = mid + 1
-        elif arr[mid] > x: #if x greater dont look left
+        elif arr[mid] > x:  #if x greater dont look left
             high = mid -1
-        else: #if x smaller dont look right
+        else:               #if x smaller dont look right
             return mid
     return -1
 
 
-#last line
+
--- dsa/challenges/array_reverse/array_reverse.py
+++ dsa/challenges/array_reverse/array_reverse.py
@@ -1,2 +1,26 @@
+
+
 def array_reverse(arr):
     return arr[::-1]
+
+
+#
+#   [ start : end ]
+#
+#   H  E  L  L  O
+#   1  2  3  4  5
+#  -5 -4 -3 -2 -1
+#
+#   [ 1 : 4 ] >>  E L L
+#   [-4 : -1] >>  E L L
+#
+#
+#
+#  S[a:b:d]  third parameter specifies the step // 2everyother -1reverse
+#
+#   [ start : end : step ]
+#
+#   [::2]   >> H L O
+#
+#   [::-1]  >> O L L E H
+#
